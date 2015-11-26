9dda3eaeaf36ee8b5e0ed2a427c16d30084bc4e9
Pascal <Mirrorbreak@users.noreply.github.com>
2015-11-26 20:34:46 +0100

    Update discord_bot.js

--- discord_bot.js
+++ discord_bot.js
@@ -4,7 +4,7 @@
   Everytime a message matches a command, the bot will respond.
 ========================
 */
-var version = "1.1.0";
+var version = "1.1.2";
 
 var Discord = require("discord.js");
 
